21e8da485dba462e70f910c286f1e355598ccec6
benz <benzvanbouwel12@gmail.com>
2024-06-11 00:26:53 +0200

    modified

--- main.js
+++ main.js
@@ -4,3 +4,13 @@ onload = () => {
       clearTimeout(c);
     }, 1000);
   };
+  const numberOfStars = 100; // Adjust the number of stars as needed
+
+for (let i = 0; i < numberOfStars; i++) {
+  const star = document.createElement('div');
+  star.classList.add('star');
+  star.style.top = `${Math.random() * 100}%`;
+  star.style.left = `${Math.random() * 100}%`;
+  star.style.animationDelay = `${Math.random() * 10}s`; // Randomize animation delay
+  document.querySelector('.stars').appendChild(star);
+}
